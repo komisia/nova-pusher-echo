9279187e2d7218125cf5eca1f0d93bea142e8aec
komisia <berikelashvili@gmail.com>
2023-09-14 13:53:52 +0400

    fix pusher 8 ws connection

--- resources/js/nova-echo.js
+++ resources/js/nova-echo.js
@@ -2,47 +2,41 @@ import Echo from 'laravel-echo'
 
 window.Pusher = require('pusher-js')
 
-let echoOptions = {
-  broadcaster: document.head.querySelector('meta[name="broadcaster"]').content
-}
+if ( document.head.querySelector('meta[name="broadcaster"]') ) {
 
-if (document.head.querySelector('meta[name="host"]') !== null) {
-  echoOptions.host = document.head.querySelector('meta[name="host"]').content
-}
+  let echoOptions = {
+    broadcaster: document.head.querySelector('meta[name="broadcaster"]').content,
+    enabledTransports: ['ws', 'wss']
+  }
 
-if (document.head.querySelector('meta[name="pusher_key"]') !== null) {
-  echoOptions.key = document.head.querySelector('meta[name="pusher_key"]').content
-}
+  if (document.head.querySelector('meta[name="pusher_key"]') !== null) {
+    echoOptions.key = document.head.querySelector('meta[name="pusher_key"]').content
+  }
+  if (document.head.querySelector('meta[name="pusher_cluster"]') !== null) {
+    echoOptions.cluster = document.head.querySelector('meta[name="pusher_cluster"]').content
+  }
+  if (document.head.querySelector('meta[name="pusher_wsHost"]') !== null) {
+    echoOptions.wsHost = document.head.querySelector('meta[name="pusher_wsHost"]').content
+  }
+  if (document.head.querySelector('meta[name="pusher_wsPort"]') !== null) {
+    echoOptions.wsPort = document.head.querySelector('meta[name="pusher_wsPort"]').content
+  }
+  if (document.head.querySelector('meta[name="pusher_wssPort"]') !== null) {
+    echoOptions.wssPort = document.head.querySelector('meta[name="pusher_wssPort"]').content
+  }
+  if (document.head.querySelector('meta[name="pusher_forceTLS"]') !== null) {
+    echoOptions.forceTLS = document.head.querySelector('meta[name="pusher_forceTLS"]').content
+  }
 
-if (document.head.querySelector('meta[name="pusher_cluster"]') !== null) {
-  echoOptions.cluster = document.head.querySelector('meta[name="pusher_cluster"]').content
-}
 
-if (document.head.querySelector('meta[name="pusher_host"]') !== null) {
-  echoOptions.wsHost = document.head.querySelector('meta[name="pusher_host"]').content
-}
+  window.Echo = new Echo(echoOptions);
 
-if (document.head.querySelector('meta[name="pusher_port"]') !== null) {
-  echoOptions.wsPort = document.head.querySelector('meta[name="pusher_port"]').content
-}
 
-if (document.head.querySelector('meta[name="pusher_port"]') !== null) {
-  echoOptions.wssPort = document.head.querySelector('meta[name="pusher_port"]').content
-}
 
-if (document.head.querySelector('meta[name="pusher_encrypted"]') !== null) {
-  echoOptions.encrypted = (document.head.querySelector('meta[name="pusher_encrypted"]').content === '1')
-}
+  let userReceivesBroadcastOn = document.head.querySelector('meta[name="user_private_channel"]').content
 
-// Check if we need to define auth endpoint
-if (document.head.querySelector('meta[name="auth_endpoint"]') !== null) {
-  echoOptions.authEndpoint = document.head.querySelector('meta[name="auth_endpoint"]').content
-}
+  if (userReceivesBroadcastOn !== null) {
+    window.userPrivateChannel = window.Echo.private(userReceivesBroadcastOn)
+  }
 
-window.Echo = new Echo(echoOptions)
-
-let userReceivesBroadcastOn = document.head.querySelector('meta[name="user_private_channel"]').content
-
-if (userReceivesBroadcastOn !== null) {
-  window.userPrivateChannel = window.Echo.private(userReceivesBroadcastOn)
 }
--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -1,10 +1,6 @@
-@if(request()->user())
+@if(request()->user() and request()->user()->pusher )
 
-    <meta name="broadcaster" content="{{ config('broadcasting.connections.' . config('broadcasting.nova', config('broadcasting.default')) . '.broadcaster', config('broadcasting.nova', config('broadcasting.default'))) }}">
-
-    @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('host')))
-      <meta name="host" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('host') }}">
-    @endif
+    <meta name="broadcaster" content="pusher">
 
     @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('key')))
       <meta name="pusher_key" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('key') }}">
@@ -14,21 +10,23 @@
       <meta name="pusher_cluster" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.cluster') }}">
     @endif
 
-    @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('options.encrypted')))
-      <meta name="pusher_encrypted" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.encrypted') ? 1 : 0 }}">
-    @endif
-
     @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('options.host')))
-      <meta name="pusher_host" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.host') }}">
+      <meta name="pusher_wsHost" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.host') }}">
     @endif
 
     @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('options.port')))
-      <meta name="pusher_port" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.port') }}">
+      <meta name="pusher_wsPort" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.port') }}">
     @endif
 
-    @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('auth_endpoint')))
-      <meta name="auth_endpoint" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('auth_endpoint') }}">
+    @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('options.port')))
+      <meta name="pusher_wssPort" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.port') }}">
     @endif
+
+    @if(!is_null(\Komisia\NovaPusherEcho\NovaPusherEcho::config('options.useTLS')))
+      <meta name="pusher_forceTLS" content="{{ \Komisia\NovaPusherEcho\NovaPusherEcho::config('options.useTLS') }}">
+    @endif
+
+
 
     @if(method_exists(request()->user(), 'receivesBroadcastNotificationsOn'))
       <meta name="user_private_channel" content="{{ request()->user()->receivesBroadcastNotificationsOn() }}">
